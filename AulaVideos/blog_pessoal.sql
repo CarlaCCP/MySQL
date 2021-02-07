8a4314b1acae4e63d540feb71c1f571b84675cd2
Carla Cristina <carldepaula230@gmail.com>
2021-02-07 11:10:33 -0300

    introdução a mysql e sql

---
+++ AulaVideos/blog_pessoal.sql
@@ -0,0 +1,10 @@
+use blog_pessoal3;
+show tables;
+select * from postagem;
+insert into postagem (id, ano, data, texto, titulo, tema_id)values (null, 2010, null, "Dúvida", "solicitações", 2);
+insert into postagem (id, ano, data, texto, titulo, tema_id)values (null, 2011, null, "Preço", "solicitações", 2);
+insert into postagem (id, ano, data, texto, titulo, tema_id) values (null, 2012, null, "Preço", "solicitações", 2);
+insert into postagem (id, ano, data, texto, titulo, tema_id) values (null, 2013, null, "Preço", "solicitações", 2);
+insert into postagem (id, ano, data, texto, titulo, tema_id)values (null, 2014, null, "Preço", "solicitações", 2);
+
+ 
