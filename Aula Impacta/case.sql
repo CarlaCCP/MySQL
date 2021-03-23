fae68f52434e01d17c5e82cf48af6c0eddefe42f
Carla Cristina <carldepaula230@gmail.com>
2021-03-23 13:08:41 -0300

    Case

---
+++ Aula Impacta/case.sql
@@ -0,0 +1,25 @@
+use relacionamentos;
+select * from funcionarios;
+
+select fun_nome as "Funcionarios",
+		case fun_sexo
+			when 'm' then "Masculino"
+			when "f" then "Pessoas incriveis"
+			else "Não informado"
+        end 
+        as "Sexo"
+from funcionarios;
+
+-- quando o valor for null
+
+select 
+		fun_nome as "Funcionario",
+		case
+        when car_nome is null then "Não informado"
+        when car_nome is not null then car_nome
+        end as "Cargo"
+        from 
+	funcionarios f
+left join 
+	cargos c on 
+    f.car_id = c.car_id;
